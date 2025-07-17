9ff64f914117fc818fd79c4956f5f5b296bf1db4
MikelUrle <UrlezagaMikel@gmail.com>
2025-07-18 00:09:05 +0200

    primeros pasos

---
+++ controllers/obtener_datos.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/json');
+include '../db/conexion.php';
+
+$sql = "SELECT id, nombre, correo, foto FROM datos_propios";
+$result = $conn->query($sql);
+
+$datos = [];
+
+if ($result && $result->num_rows > 0) {
+    while ($fila = $result->fetch_assoc()) {
+        $datos[] = $fila;
+    }
+}
+
+if (!$result) {
+    echo json_encode(['error' => 'Error en la consulta: ' . $conn->error]);
+} else {
+    echo json_encode($datos);
+}
+
+$conn->close();
+?>
